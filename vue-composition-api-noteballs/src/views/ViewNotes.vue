e73dc38b029722ffa300c3d58ab4f669ad3727c2
ADS\morh <morhak6819@gmail.com>
2024-02-20 22:52:02 +0200

    Init notes project

---
+++ vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="notes">
+    <div class="card has-background-success-dark mb-4 p-4">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ vue-composition-api-noteballs/src/views/ViewStats.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Stats view</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
